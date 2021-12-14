122efab51da7030a613789e06e09aa0cbeae1002
paaaarv <pkumar@trinity.edu>
2021-12-13 22:54:04 -0600

    solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(input)
+    array=[]
+    i=1
+    while i<input
+        if i%3==0 || i%5==0
+            array.push(i)
+        end
+        i+=1
+    end
+    return array
+end
+
+def sum_multiples(input)
+    counter=0
+    newArray=collect_multiples(input)
+    i=0
+
+    while i<newArray.length
+        counter+=newArray[i]
+        i+=1
+    end
+    return counter
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,31 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+    def initialize(input)
+        @start=input
+    end
+
+    def collect_multiples
+        array=[]
+        i=1
+        while i<@start
+            if i%3==0 || i%5 ==0
+                array.push(i)
+            end
+            i+=1
+        end
+        return array
+    end
+
+    def sum_multiples
+        counter=0
+        array=collect_multiples
+        i=0
+        while i<array.length
+            counter+=array[i]
+            i+=1
+        end
+        return counter
+    end
+
+end
